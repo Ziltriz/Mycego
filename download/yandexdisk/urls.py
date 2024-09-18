b4a54b17d1afd31308f1c808710c5e01ad787029
Ziltriz <77804688+Ziltriz@users.noreply.github.com>
2024-09-19 00:38:56 +0300

    day 2
    Добавил авторизацию, изменил реализацию функции скачивания файлов, добавил фильтрацию файлов по типу, добавил инструкцию

--- download/yandexdisk/urls.py
+++ download/yandexdisk/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-from .views import IndexView, download_file
-
-
+from .views import IndexView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('<str:public_key>/<str:file_path>', download_file, name='download_file'),
 ]
--- download/yandexdisk/views.py
+++ download/yandexdisk/views.py
@@ -1,27 +1,47 @@
 import requests
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views import View
-from .config import oauth_secret
+from .config import oauth_secret, filters
 
 
-class IndexView(View):
+class IndexView(LoginRequiredMixin, View):
+    login_url = 'accounts/login/'  # Страница авторизации
+    redirect_field_name = 'redirect_to'  # Поле, в которое будет передана ссылка после авторизации
 
-    def get(self, request):
+    def get(self, request):  # Переход на главную страницу
         return render(request, 'index.html')
 
     def post(self, request):
-        public_key = request.POST.get('public_key')
+
+        # Получение ссылки из формы (str)
+        public_key: str = request.POST.get('public_key')
+
+        # Получение фильтра из формы (str)
+        filter: str = request.POST.get('filter')
+
+        # Используем Yandex API для получения списка файлов с диска(json)
         url = f'https://cloud-api.yandex.net/v1/disk/public/resources?public_key={public_key}'
         headers = {f'Authorization': f'OAuth {oauth_secret}'}
         response = requests.get(url, headers=headers)
-        files = response.json()['_embedded']['items']
-        context = {'files': files, 'public_key': public_key}
+
+        # Получение списка файлов с диска из фильтра (list)
+        if filter is not None:
+            filtered_files = []
+            f = filter[:-1]
+            media_type = filters[f]
+            files = response.json()['_embedded']['items']
+            for file in files:
+                if file['media_type'] == media_type:
+                    filtered_files.append(file)
+
+            context = {'files': filtered_files, 'public_key': public_key, 'filters': filters}
+            return render(request, 'index.html', context)
+
+        # Получение списка файлов с диска без фильтра(list)
+        else:
+            files = response.json()['_embedded']['items']
+
+        # Вывод списка файлов с диска
+        context = {'files': files, 'public_key': public_key, 'filters': filters}
         return render(request, 'index.html', context)
-
-
-def download_file(request, public_key, file_path):
-    url = f'https://cloud-api.yandex.net/v1/disk/public/resources/download?public_key={public_key}&path={file_path}'
-    headers = {f'Authorization': f'OAuth {oauth_secret}'}
-    file = requests.get(url, headers=headers).json()['href']
-    response = requests.get(file)
-    return response
