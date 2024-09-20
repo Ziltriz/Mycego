480a42220562af015884e6764291dcc28a69dad0
Ziltriz <77804688+Ziltriz@users.noreply.github.com>
2024-09-20 06:41:13 +0300

    pre-final

---
+++ download/yandexdisk/config.py
@@ -0,0 +1,20 @@
+Client_ID = '' # Contact the repository owner to get the client ID
+oauth_secret = ''# Contact the repository owner to get the client secret
+filters = {
+    'Аудио-файлы': 'audio',
+    'Видео': 'video',
+    'Книги': 'book',
+    'Изображения': 'image',
+    'Документы': 'document',
+    'Таблицы': 'spreadsheet',
+    'Текстовые файлы': 'text',
+    'Файлы резервных копий': 'backup',
+    'Сжатые и архивированные файлы': 'compressed',
+    'Файлы с базами данных': 'data',
+    'Файлы с кодом': 'development',
+    'Образы носителей информации(ISO)': 'diskimage',
+    'Зашифрованные файлы': 'encoded',
+    'Исполняные файлы': 'executable',
+    'Flash файлы': 'flash',
+    'Шрифты': 'font',
+    'Настройки приложений': 'settings', }
--- download/yandexdisk/views.py
+++ download/yandexdisk/views.py
@@ -6,26 +6,26 @@ from .config import oauth_secret, filters
 
 
 class IndexView(LoginRequiredMixin, View):
-    login_url = 'accounts/login/'  # Страница авторизации
-    redirect_field_name = 'redirect_to'  # Поле, в которое будет передана ссылка после авторизации
+    login_url = 'accounts/login/'  # Login page URL
+    redirect_field_name = 'redirect_to'
 
-    def get(self, request):  # Переход на главную страницу
+    def get(self, request):  # Redirect to a success page
         return render(request, 'index.html')
 
     def post(self, request):
 
-        # Получение ссылки из формы (str)
+        # Url from form (str)
         public_key: str = request.POST.get('public_key')
 
-        # Получение фильтра из формы (str)
+        # Filter from form(str)
         filter: str = request.POST.get('filter')
 
-        # Используем Yandex API для получения списка файлов с диска(json)
+        # Get files from Yandex API(json)
         url = f'https://cloud-api.yandex.net/v1/disk/public/resources?public_key={public_key}'
         headers = {f'Authorization': f'OAuth {oauth_secret}'}
         response = requests.get(url, headers=headers)
 
-        # Получение списка файлов с диска из фильтра (list)
+        # Filter files (list)
         if filter is not None:
             filtered_files = []
             f = filter[:-1]
@@ -38,10 +38,10 @@ class IndexView(LoginRequiredMixin, View):
             context = {'files': filtered_files, 'public_key': public_key, 'filters': filters}
             return render(request, 'index.html', context)
 
-        # Получение списка файлов с диска без фильтра(list)
+        # Files without filter(list)
         else:
             files = response.json()['_embedded']['items']
 
-        # Вывод списка файлов с диска
+        # Render files
         context = {'files': files, 'public_key': public_key, 'filters': filters}
         return render(request, 'index.html', context)
